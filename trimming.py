94aad19b12a4597be54c8905e2371000e9226e1a
YoshieSuzuki <futurekame@gmail.com>
2022-04-10 07:13:22 +0900

    fix bug

--- trimming.py
+++ trimming.py
@@ -8,18 +8,18 @@ from pydub import AudioSegment
 
 
 def trimming():
-	json_list = []
+	row_list = []
 	audio_data = AudioSegment.from_mp3('./audio_tmp/tmp.mp3')
 
 	seg = Segmenter(vad_engine='smn', detect_gender=False)
-	segmentation = seg(audio_data)
+	segmentation = seg('./audio_tmp/tmp.mp3')
 	seg2csv(segmentation, './audio_tmp/Result.csv')
 
 	with open('./audio_tmp/Result.csv', 'r') as f:
-		for row in csv.DictReader(f):
-			json_list.append(row)
+		for row in csv.DictReader(f, delimiter="\t",):
+			row_list.append(row)
 
 	os.remove('./audio_tmp/tmp.mp3')
 	os.remove('./audio_tmp/Result.csv')
 
-	return json.dumps(json_list)
+	return json.dumps(row_list)
